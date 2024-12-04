16ac7e39004f3f71fe1b264ff1e432ed64a3a53f
Giovanni-Peirone <mgpeirone25@gmail.com>
2024-12-03 21:17:12 -0300

    structure

---
+++ lib-install/linux/sudo.sh
@@ -0,0 +1,7 @@
+sudo apt update
+
+sudo apt install build-essential g++
+
+sudo apt install build-essential cmake xorg-dev libglfw3 libglfw3-dev libglew-dev
+
+sudo apt install libgl-dev libglfw3-dev
